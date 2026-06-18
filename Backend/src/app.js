01b95e14fa68127e9fa7459e47bc6ea0b698c1b1
LochanPant06 <pantlochan11@gmail.com>
2026-06-18 15:39:49 +0530

    cors setup

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,25 +1,18 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import studentRouter from "./routes/student.route.js";
 import adminRouter from "./routes/admin.route.js";
 import complaintRoutes from "./routes/complaint.routes.js";
 
 const app = express();
 
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   }),
-// );
-
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "modified-hostel-complain-management-system-35ql4m573.vercel.app",
+      "https://modified-hostel-complain-management-system-35ql4m573.vercel.app",
     ],
     credentials: true,
   })
@@ -28,6 +21,7 @@ app.use(
 app.use(cookieParser());
 
 app.use(express.json({ limit: "16kb" }));
+
 app.use(
   express.urlencoded({
     extended: true,
